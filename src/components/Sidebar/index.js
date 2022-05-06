7ae7d7c3b0f6cc25ea65035ef02abc74868361ca
hvs15 <huynhsi999@gmail.com>
2022-05-06 10:27:30 +0700

    portfolio

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,50 @@
+import './index.scss';
+import {Link, NavLink} from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome, faEnvelope, faUser, faHeartCirclePlus,faHelicopter,faHiking,faHelmetUn } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/' >
+            <img src={LogoS} alt='logo' />
+            <img className='sub-logo' src={LogoSubtitle} alt='slobodan' />
+        </Link> 
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className='about-link' to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className='contact-link' to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/">
+                    <FontAwesomeIcon icon={faHeartCirclePlus} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/">
+                    <FontAwesomeIcon icon={faHelicopter} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/">
+                    <FontAwesomeIcon icon={faHiking} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/">
+                    <FontAwesomeIcon icon={faHelmetUn} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
